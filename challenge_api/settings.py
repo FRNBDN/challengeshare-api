b63c08a5c27bfc54ee48c3aa7de945b863565d50
FRNBDN <francesco.badini94@gmail.com>
2023-03-20 07:00:57 +0000

    fix remove indent on allowed_hosts var

--- challenge_api/settings.py
+++ challenge_api/settings.py
@@ -64,7 +64,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
- ALLOWED_HOSTS = ['localhost', 'challenge-apimk3.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'challenge-apimk3.herokuapp.com']
 
 
 # Application definition
