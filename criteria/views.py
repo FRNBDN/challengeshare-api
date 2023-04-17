44cefbf7a6d00abb0ff82a37f1e90dd7c15e781a
FRNBDN <francesco.badini94@gmail.com>
2023-04-17 13:33:59 +0000

    add filter function to crtieria based on challenge

--- criteria/views.py
+++ criteria/views.py
@@ -2,12 +2,20 @@ from rest_framework import generics, permissions
 from challenge_api.permissions import IsOwnerOrReadOnly
 from .models import Criteria
 from .serializers import CriteriaSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CriteriaList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Criteria.objects.all()
     serializer_class = CriteriaSerializer
+
+    filter_backends = [
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'challenge'
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
