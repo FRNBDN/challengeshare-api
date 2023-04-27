4bf2c390f1ecc766ea47fa94c8ab1ffeda74261e
FRNBDN <francesco.badini94@gmail.com>
2023-04-27 13:52:22 +0000

    add review filter for submission

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from challenge_api.permissions import IsOwnerOrReadOnly
 from .models import Review
 from .serializers import ReviewSerializer, ReviewDetailSerializer
@@ -8,6 +9,12 @@ class ReviewList(generics.ListCreateAPIView):
     serializer_class = ReviewSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Review.objects.all()
+    filter_backends = [
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'submission',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
