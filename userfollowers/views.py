da75e86f182e201fe7cb3c86c2b1e38e7698b39f
FRNBDN <francesco.badini94@gmail.com>
2023-05-05 13:01:31 +0000

    add userfollowers filterset fields

--- userfollowers/views.py
+++ userfollowers/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from challenge_api.permissions import IsOwnerOrReadOnly
 from .models import UserFollower
 from .serializers import UserFollowerSerializer
@@ -8,6 +9,14 @@ class UserFollowerList(generics.ListCreateAPIView):
     serializer_class = UserFollowerSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = UserFollower.objects.all()
+
+    filter_backends = [
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__profile',
+        'followed__profile'
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
