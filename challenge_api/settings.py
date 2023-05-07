69730668ffc0ffc0b021ebd619e8894d7debbe7b
FRNBDN <francesco.badini94@gmail.com>
2023-05-07 16:14:48 +0000

    remove password validators

--- challenge_api/settings.py
+++ challenge_api/settings.py
@@ -165,20 +165,6 @@ else:
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
 
 
 # Internationalization
